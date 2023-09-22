066bbb63adae6936c6221398802b2e9662fb394a
FunsoJoba <hrfunsojoba@gmail.com>
2023-09-22 23:41:43 +0100

    rough work

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http")
+const fs = require("fs");
 
 const server = http.createServer((req, res)=>{
     console.log("request mode")
@@ -12,3 +13,11 @@ const server = http.createServer((req, res)=>{
 server.listen(3000, "localhost", ()=>{
     console.log("listening on port 3000")
 })
+
+
+const readStream = fs.createReadStream("mysql_.txt", {encoding:'utf-8'});
+
+// readStream.on('data', (chunk)=>{
+//     console.log(" ------ NEW CHUNK -------")
+//     console.log(chunk)
+// })
